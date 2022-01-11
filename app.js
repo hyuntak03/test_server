73456ef0233fc893ad494d84e49b1a79e825401f
hyuntak <hyuntak03.lee@gmail.com>
2022-01-12 02:00:30 +0900

    modified

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("test");
+var express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
+var app = express(); //express를 실행하여 app object를 초기화 합니다.
+
+const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
+// console.log(rows[0])
+
+app.get('/', function(req, res) { // '/' 위치에 'get'요청을 받는 경우,
+  res.send(rows); // "Hello World!"를 보냅니다.
+});
+
+app.listen(8080, function(){ 
+});
