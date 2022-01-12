1c6fb308d5edcdb41d7d9a3c637428c1d4ea1cd3
hyuntak <hyuntak03.lee@gmail.com>
2022-01-12 23:02:16 +0900

    modified

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const rows = [
 // console.log(rows[0])
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Origin", "https://localhost:3000")
   res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
