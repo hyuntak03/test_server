9ba8f6ff15131f29f58c9c91e91537fe2fc6d04a
hyuntak <hyuntak03.lee@gmail.com>
2022-01-12 23:18:30 +0900

    modified

--- app.js
+++ app.js
@@ -13,19 +13,6 @@ const rows = [
     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*")
-//   res.header(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   if (req.method === 'OPTIONS') {
-//       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-//       return res.status(200).json({});
-//   }
-//   next();
-// })
-
 app.get('/data', function(req, res) { 
   res.send(rows);
 });
