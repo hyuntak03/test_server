963a6980ef028f773e6f5dc21899c3d479eaf97c
hyuntak <hyuntak03.lee@gmail.com>
2022-01-13 09:52:42 +0900

    modified

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
-var app = express(); //express를 실행하여 app object를 초기화 합니다.
+var express = require('express');
+var app = express();
 
 const rows = [
     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
